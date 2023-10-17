5f1a1a9a63555fb30cf5c867a984b79099daa03d
Curran <curran.kelleher@gmail.com>
2023-10-17 13:29:53 -0400

    Fix build finally

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from "vite";
 
 // Inspired by
 // https://github.com/mycelial/mycelial-js/issues/25#issuecomment-1533305723
-const wasmContentTypePlugin = {
-  name: "wasm-content-type-plugin",
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
-      if (req.url.endsWith(".wasm")) {
-        res.setHeader("Content-Type", "application/wasm");
-      }
-      next();
-    });
-  },
-};
+// Turns out this was not needed, but I'm leaving it here for reference
+// const wasmContentTypePlugin = {
+//   name: "wasm-content-type-plugin",
+//   configureServer(server) {
+//     server.middlewares.use((req, res, next) => {
+//       if (req.url.endsWith(".wasm")) {
+//         res.setHeader("Content-Type", "application/wasm");
+//       }
+//       next();
+//     });
+//   },
+// };
 
+//  plugins: [wasmContentTypePlugin],
 export default defineConfig({
-  plugins: [wasmContentTypePlugin],
+  optimizeDeps: {
+    exclude: ["@rollup/browser"],
+  },
 });
